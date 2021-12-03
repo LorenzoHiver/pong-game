a1925f99b7d84f268273f5245c080a5dafd6be45
LorenzoHiver <lorenzohiver2@gmail.com>
2021-12-03 02:11:27 +0100

    feat(api): change multiples pages and creates multplies routes

---
+++ api/server/models/matchs.js
@@ -0,0 +1,16 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Matchs extends Model {};
+  Matchs.init({
+    firstPseudo: DataTypes.STRING,
+    secondPseudo: DataTypes.STRING,
+    score: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Matchs',
+  });
+  return Matchs;
+};
