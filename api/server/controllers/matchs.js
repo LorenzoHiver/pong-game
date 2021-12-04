061bd2ca36d2a3c70265b7cada0496f7d8918de4
LorenzoHiver <lorenzohiver2@gmail.com>
2021-12-04 17:19:40 +0100

    fix(api): add mutiples logs

--- api/server/controllers/matchs.js
+++ api/server/controllers/matchs.js
@@ -6,10 +6,12 @@ module.exports = {
             const { firstPseudo, secondPseudo } = req.body
             if (!firstPseudo || !secondPseudo) throw 'undefined_params'
             const match = await Matchs.create({ firstPseudo: firstPseudo, secondPseudo: secondPseudo })
+            console.log('New match created for', firstPseudo, 'and', secondPseudo)
             res.send(200, {
                 id: match.id
             })
         } catch (e) {
+            console.error('Error in match creation for:',firstPseudo, secondPseudo, e)
             res.send(400, e)
         }
 
@@ -20,6 +22,7 @@ module.exports = {
             if(!matchs) throw 'no matchs'
             res.send(200, matchs)
         } catch (e) {
+            console.error('Error in getAll matches', e)
             res.send(400, e)
         }
     },
@@ -29,8 +32,10 @@ module.exports = {
             if(!score || !id) throw 'undefined_params'
             const match = await Matchs.findByPk(id)
             await match.update({ score: score })
+            console.log('Score updated for match id:', id)
             res.send(200, match)
         } catch (e) {
+            console.error('Error in uptaded score for match:',id, e)
             res.send(400, e)
         }
     },
